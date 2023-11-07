79af62b8edb53bb152e9a4d1e7e99d7e59e8642c
Piyush Kumar <piyush20152003@gmail.com>
2023-11-07 16:37:01 +0530

    smol change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
+          <Route path="/product" element={<Productpage />} />
         </Routes>
       </HashRouter>
     </div>
