7ce1fede71e178188edaddbf7575b9cc7ab5d6e7
Radhika <radhika.sardeshpande258@gmail.com>
2023-11-09 15:59:47 +0530

    Update backend for Signup

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 const PORT = 5555;
-const dburl= "mongodb+srv://friedcheesee:abcde@cluster0.vqdpm1s.mongodb.net/bike"
-module.exports = {PORT,dburl};
+const usersDbUrl = "mongodb+srv://friedcheesee:abcde@cluster0.vqdpm1s.mongodb.net/Users"
+const bikeDbUrl = "mongodb+srv://friedcheesee:abcde@cluster0.vqdpm1s.mongodb.net/bike"
+module.exports = {PORT, usersDbUrl, bikeDbUrl};
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 express = require('express');
 mongoose = require('mongoose');
-mongoose.set("strictQuery",true);
+mongoose.set("strictQuery", true);
 const cors = require("cors"); //allow frontend to access backend
 const bodyParser = require("body-parser");
-const { PORT, dburl } = require('./config.js');
+const { PORT, usersDbUrl, bikeDbUrl } = require('./config.js');
 
 const prodRoute = require("./controller/prodRoute.js");//route to get all products or specific ones
+const authRoutes = require('./controller/authRoutes.js');
 
 const app = express();
 
@@ -14,20 +15,40 @@ app.use(bodyParser.urlencoded({ extended: true }));//to get id from url
 app.use(cors());
 
 app.use("/products", prodRoute);
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
-    });
+});
 
 //Just to check if server is working, visit http://localhost:5555/
 app.get("/", (req, res) => {
     return res.status(200).json({ message: "Express app homepage" });
-    });
+});
 
-mongoose
-    .connect(dburl).then(() => {
-    console.log("Connected to DB");
-    }
-    ).catch((error) => {
+
+const connectUsersDB = async () => {
+    try {
+        await mongoose.connect(usersDbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to Users DB");
+    } catch (error) {
         console.log(error);
-    });
+    }
+};
+
+const connectBikeDB = async () => {
+    try {
+        const bikeConnection = mongoose.createConnection(bikeDbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+        bikeConnection.on('open', () => {
+            console.log('Connected to Bike DB');
+        });
+        bikeConnection.on('error', (error) => {
+            console.log(error);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connectUsersDB();
+connectBikeDB();
