cc23805027cb87f863387edf8f1b577faa20b86f
Piyush Kumar <piyush20152003@gmail.com>
2023-11-17 19:13:37 +0530

    added prod page back

--- frontend/src/components/ProductView.js
+++ frontend/src/components/ProductView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import RatingStars from './RatingStars';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
