f755222e1d5975a35559c58194ada7de715a9686
Radhika <radhika.sardeshpande258@gmail.com>
2023-11-19 17:35:23 +0530

    Update login, signup pages

--- backend/controller/authRoutes.js
+++ backend/controller/authRoutes.js
@@ -1,63 +1,26 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('../models/User'); // Import the User model
 const router = express.Router();
+
+const userSchema = require("../models/userSchema");
+const usersDbUrl = mongoose.createConnection("mongodb+srv://friedcheesee:abcde@cluster0.vqdpm1s.mongodb.net/Users?retryWrites=true&w=majority");
+const User = usersDbUrl.model("User", userSchema);
 
 // Signup route
 router.post('/signup', async (req, res) => {
-  try {
-    const { firstName, lastName, email, phoneNumber, userName, password, address } = req.body;
+    try {
+        // Extract user data from the request body
+        const { firstName, lastName, email, phoneNumber, userName, password, address } = req.body;
 
-    const existingUser = await User.findOne({ $or: [{ email }, { userName }] });
+        // Create a new user in the database
+        const newUser = new User({ firstName, lastName, email, phoneNumber, userName, password, address });
+        await newUser.save();
 
-    if (existingUser) {
-      return res.status(400).json({ success: false, message: 'Email or username already exists' });
+        // Respond with a success message
+        res.status(201).json({ message: 'Signup successful' });
+    } catch (error) {
+        // Handle errors (e.g., duplicate username or email)
+        res.status(400).json({ error: error.message });
     }
-
-    // Hash the password before saving to the database
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      userName,
-      password: hashedPassword,
-      address,
-    });
-
-    await newUser.save();
-
-    res.json({ success: true, message: 'User registered successfully' });
-  } catch (error) {
-    console.error('Error during signup:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-});
-
-// Login route
-router.post('/login', async (req, res) => {
-  try {
-    const { userName, password } = req.body;
-
-    const user = await User.findOne({ userName });
-
-    if (!user) {
-      return res.status(401).json({ success: false, message: 'Invalid user' });
-    }
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({ success: false, message: 'Invalid password' });
-    }
-
-    res.json({ success: true, message: 'Login successful' });
-  } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
 });
 
 // Login route
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,72 +1,7 @@
-// import React from 'react';
-// import { Link } from "react-router-dom";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faUser, faLock, faMotorcycle } from '@fortawesome/free-solid-svg-icons';
-// // import './Login.css';
-// import './Signup.css';
-
-// function Login() {
-//   return (
-
-//     <div>
-//       <div>
-//         <nav className="navbar row">
-//           <div className="align-items-center">
-//               <Link className="navbar-brand" to="/">
-//                 <h1 className="pt-3">Bikeswale <FontAwesomeIcon icon={faMotorcycle} /> </h1>
-//               </Link>
-//           </div>
-//         </nav>
-//       </div>
-//       <div className="main_div">
-//         <h2>Bikeswale</h2>
-//         <form action="#">
-//           <div className="input_box">
-//             <input type="text" placeholder="  Username" required />
-//             <div className="icon">
-//               <FontAwesomeIcon icon={faUser} />
-//             </div>
-//           </div>
-//           <div className="input_box">
-//             <input type="password" placeholder="  Password" required />
-//             <div className="icon">
-//               <FontAwesomeIcon icon={faLock} />
-//             </div>
-//           </div>
-//           <div className="btnloginsignup">
-//             <input  type="submit" value="Login" />
-//           </div>
-//           <br />
-//           <div className="option_div">
-//             <div className="check_box">
-//               <input type="checkbox" />
-//               <span style={{ marginLeft: "2vh", }}>Remember me</span>
-//             </div>
-//             <div className="forget_div">
-//               <a href="#">Forgot Password?</a>
-//             </div>
-//           </div>
-//           <br />
-//           <div className="sign_up">
-//             Don't have an account?
-//             <Link className="nav-link" to="/signup" id="signup">
-//               Sign up here!
-//             </Link>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faLock, faMotorcycle } from '@fortawesome/free-solid-svg-icons';
-// import './Login.css';
 import './Signup.css';
 
 function Login() {
@@ -121,7 +56,6 @@ function Login() {
         </nav>
       </div>
       <div className="main_div">
-        {/* <h2>Bikeswale</h2> */}
         <form onSubmit={handleSubmit}>
           <div className="input_box">
             <input
