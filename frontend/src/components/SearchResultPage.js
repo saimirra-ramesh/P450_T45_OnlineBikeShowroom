0a401297c56154e78b855f93e0ebc491d43072eb
adising <aditising.as@gmail.com>
2023-11-23 14:44:32 +0530

    Beautified search results

--- frontend/src/components/SearchResultPage.js
+++ frontend/src/components/SearchResultPage.js
@@ -1,7 +1,8 @@
-import { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Nav from './Nav.js';
 import ProductView from './ProductView.js';
+import { Card, Col, Container, Row, Button } from 'react-bootstrap';
 
 function SearchResultPage({ onSearch }) {
   const [searchResults, setSearchResults] = useState('');
@@ -21,7 +22,7 @@ function SearchResultPage({ onSearch }) {
 
       setSearchResults(data);
       setQuery(query);
-      console.log('SearchResultPage.js, Search data:', data); // Using the 'data' variable directly
+      console.log('SearchResultPage.js, Search data:', data);
       console.log('SearchResultPage.js, Query Received: ', query);
     } catch (error) {
       console.error("SearchResultPage.js, Error fetching data:", error);
@@ -47,14 +48,38 @@ function SearchResultPage({ onSearch }) {
     content = (
       <div id="search-results">
         <h2 style={{ paddingTop: "15px" }}>Search Results</h2>
-        {searchResults.map((result) => (
-          <div key={result._id}>
-            <button id="search-results" onClick={() => handleViewProduct(result)}>
-              {result.name}
-            </button>
-            {selectedProduct === result && <ProductView product={result} />}
-          </div>
-        ))}
+        <Container>
+          <Row xs={1} md={2} lg={4} className="g-4">
+            {searchResults.map((result) => (
+              <Col key={result._id} xs={12} md={6} lg={3}>
+                <Card
+                  style={{
+                    height: '100%',
+                    backgroundColor: 'black',
+                    color: 'white',
+                    border: '15px solid black',
+                    marginBottom: '20px', 
+                  }}
+                  onClick={() => handleViewProduct(result)}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={result.imageUrl}
+                    style={{ objectFit: 'cover', height: '60%', minHeight: '150px' }}
+                  />
+                  <Card.Body>
+                    <Card.Title>{result.name}</Card.Title>
+                    <Card.Text>Cost: {result.price} Lakhs</Card.Text>
+                    <p></p>
+                    <Button variant="dark" style={{ backgroundColor: 'red', color: 'white' }} onClick={() => handleViewProduct(result)}>
+                      View Product
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
         <hr />
       </div>
     );
